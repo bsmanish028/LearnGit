8641e59058fba464a652e2fbd6bf411c740c04bb
Manish Kumar <manishkr201331@gmail.com>
2018-08-05 00:23:43 +0530

    JavaScript File added

---
+++ work_with_string.js
@@ -0,0 +1,18 @@
+
+console.log("ABc".length);
+console.log("I am a String".length); // get the length of the string
+
+var str = "My name is Bravia";
+console.log("The original string is : " + str);
+var uprcase = str.toUpperCase(); // change to upper case
+console.log("This transformed string is : " + uprcase);
+console.log("The transformed string is : " + uprcase.toLowerCase()); // change to lower case
+
+
+
+var name = prompt("Enter name : ");
+for(var i=0; i<name.length; i++){
+	console.log(name[i]);
+}
+
+
